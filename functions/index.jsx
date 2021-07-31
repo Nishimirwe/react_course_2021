2bf08e73af1022fe24e295b3bbc9c42e922a8d63
Nishimirwe <nishimirwepaul20105@gmail.com>
2021-07-31 18:11:05 +0200

    Starting with React

---
+++ functions/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const myBio = React.createElement('div', null , 'My name is Jean Paul, and I love Rwanda.');
--- functions/my_check.jsx
+++ functions/my_check.jsx
@@ -37,9 +37,43 @@ function map1() // map with objects, how to deal with it.
         { artist: 'Justin Bieber', name: 'Purpose', sales: 554000 }
     ];
     
+    // The following variable is going to cancatenate strings as
+    /*
+    <name> by <artist> sold <sales> copies
+    */
+    document.write("<h1>Display Data in format of --name-- by --artist-- sold --sales-- copies </h1><br>");
     const albumSalesStrings = musicData.map(musicData=>musicData.name+ " by "+ musicData.artist+ " sold "+musicData.sales+ " copies ");
     for(let k=0;k<albumSalesStrings.length;k++)
     {
         document.write(albumSalesStrings[k]+ "<br>");
+    }
+    document.write("<br><br>")
+    /*
+    	I am going to make another variable using "filter react function"
+        This variable will filter in the array all album names whose length >10 and <=25
+    */
+   document.write("<h1>All albums whose name length is between 10 and 25</h1><br>");
+   const results=musicData.filter(data=>data.name.length>=10 && data.name.length<=25);
+   for(let i=0;i<results.length;i++)
+   {
+       document.write(results[i].name+ "<br>")
+   }
+   document.write("<br><br>")
+   document.write("<h1>All albums whose name length is between 10 and 25</h1><br>");
+   const greater=musicData.filter(data=>data.sales>=1000000).map(mdata=>mdata.artist+' is a great performer because they have '+ mdata.sales);
+   for(let j=0;j<greater.length;j++)
+   {
+        document.write(greater[j]+"<br><br>");
+   }
+}
+
+// function to combine both filter function() and map()
+function filter_map()
+{
+    const names = ['Karen', 'Richard', 'Tyler'];
+    const shortNamesLengths = names.filter( data => data.length < 8 ).map(data=> data+ " is small" );
+    for(let i=0;i<shortNamesLengths.length;i++)
+    {
+        document.write(shortNamesLengths[i]+"<br><br>");
     }
 }
