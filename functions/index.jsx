fd1538c4a392729ba53cbfcb9469df0aba38c38c
Nishimirwe <nishimirwepaul20105@gmail.com>
2021-08-01 11:14:04 +0200

    Creating a new React project

--- functions/index.jsx
+++ functions/index.jsx
@@ -2,3 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const myBio = React.createElement('div', null , 'My name is Jean Paul, and I love Rwanda.');
+const element=React.createElement('div',null,'Hello world')
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+)
